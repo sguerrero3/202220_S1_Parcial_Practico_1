07fec07d122fea12e67e55b004a940ec126270b5
sguerrero3 <s.guerrero3@uniandes.edu.co>
2022-09-23 07:58:01 -0500

    a

--- src/main/java/co/edu/uniandes/dse/parcialejemplo/services/HabitacionService.java
+++ src/main/java/co/edu/uniandes/dse/parcialejemplo/services/HabitacionService.java
@@ -1,5 +1,55 @@
 package co.edu.uniandes.dse.parcialejemplo.services;
 
+import java.util.Optional;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import co.edu.uniandes.dse.parcialejemplo.entities.HabitacionEntity;
+import co.edu.uniandes.dse.parcialejemplo.entities.HotelEntity;
+import co.edu.uniandes.dse.parcialejemplo.exceptions.IllegalOperationException;
+import co.edu.uniandes.dse.parcialejemplo.repositories.HabitacionRepository;
+import co.edu.uniandes.dse.parcialejemplo.repositories.HotelRepository;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
 public class HabitacionService {
+
+    @Autowired
+    HabitacionRepository habitacionRepository;
+
+    @Autowired
+    HotelRepository hotelRepository;
+
+    /**
+     * @param hotelId
+     * @param habitacionEntity
+     * @return
+     * @throws IllegalOperationException
+     */
+    @Transactional
+    public HabitacionEntity createHabitacion(Long hotelId, HabitacionEntity habitacionEntity) throws IllegalOperationException{
+
+        log.info("Inicia proceso creacion habitacion");
+
+        if (habitacionEntity.getNumero_banios() > habitacionEntity.getNumero_personas())
+
+            throw new IllegalOperationException("Mayor numero de banios quye personas");
+        
+
+        Optional<HotelEntity> hotelEntity = hotelRepository.findById(hotelId);
+
+        if (hotelEntity.isEmpty())
+			throw new IllegalOperationException("Hotel no encontrado");
+        
+        habitacionEntity.setHotel(hotelEntity.get());
+
+        return habitacionRepository.save(habitacionEntity);
+    }
+    
+
     
 }
--- src/main/java/co/edu/uniandes/dse/parcialejemplo/services/HotelService.java
+++ src/main/java/co/edu/uniandes/dse/parcialejemplo/services/HotelService.java
@@ -1,8 +1,12 @@
 package co.edu.uniandes.dse.parcialejemplo.services;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import co.edu.uniandes.dse.parcialejemplo.entities.HotelEntity;
+import co.edu.uniandes.dse.parcialejemplo.exceptions.IllegalOperationException;
 import co.edu.uniandes.dse.parcialejemplo.repositories.HotelRepository;
 import lombok.extern.slf4j.Slf4j;
 
@@ -13,7 +17,25 @@ public class HotelService {
     @Autowired
     HotelRepository hotelRepository;
 
-    
+    @Transactional
+    public HotelEntity createHotel(HotelEntity hotelEntity) throws IllegalOperationException{
+        
+        log.info("Inicia Proceso de creacion del libro ");
+
+        
+        if (!hotelRepository.findByNombre(hotelEntity.getNombre()).isEmpty())
+
+            throw new IllegalOperationException("Nombre del hotel ya existe");
+
+        if (hotelEntity.getEstrellas()<2 ||  hotelEntity.getEstrellas()>5)
+
+        throw new IllegalOperationException("El hotel no esta en el rango de estrellas requerido");
+
+
+        log.info("Termina proceso de creacion del libro");
+        return hotelRepository.save(hotelEntity);
+
+    }
     
     
 }
